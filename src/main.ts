c569d73ca803fc3779c782cfe27216761cc21df5
Brian Omondi <omondibrian392@gmail.com>
2022-07-20 18:02:57 -0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,35 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import * as express from "express";
+import * as cors from 'cors';
+import  helmet from "helmet";
+import * as morgan from "morgan";
+import {config} from 'dotenv'
+import userRoutes from "./routes/user";
+import health from "./routes/health";
+import productsRoutes from "./routes/products";
+import { TokenMiddleware } from "./utils/middlewares";
+import orderRoutes from "./routes/order";
+import payment from "./routes/payment";
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+config(); 
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
+const App = express();
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+
+App.use(express.urlencoded({ extended: true }));
+App.use(express.json());
+
+App.use("/api/uploads", express.static("uploads"));
+App.use(cors());
+App.use(helmet());
+
+App.use(morgan("tiny"));
+App.use('/health',health)
+App.use('/api/v1/auth/',userRoutes)
+App.use('/api/v1/products',TokenMiddleware,productsRoutes)
+App.use('/api/v1/orders',TokenMiddleware,orderRoutes)
+App.use('/api/v1/payment',TokenMiddleware,payment)
+
+App.listen(5000, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${5000}`);
+});
